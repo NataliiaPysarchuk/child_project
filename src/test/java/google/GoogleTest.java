9d7ecc6f3936d772d3de132883b5c78c3fae80c4
pysarchuknataliia <nataliia.pysarchuk@perfectial.com>
2021-07-19 20:02:52 +0300

    Initial commit

---
+++ src/test/java/google/GoogleTest.java
@@ -0,0 +1,12 @@
+package google;
+
+import api.GoogleClient;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+    @Test
+    public void testGetGoogle(){
+        GoogleClient.getGoogle();
+    }
+}
